d867977abcbc66df1269bbfdd037583931bd5fc7
Andromeda <andromeda@notaglitch.net>
2026-05-20 00:22:07 -0400

    Update grab-ui.rb

--- Formula/grab-ui.rb
+++ Formula/grab-ui.rb
@@ -1,8 +1,8 @@
 class GrabUi < Formula
   desc "Web interface for grab.sh - Media Automation & Management"
   homepage "https://github.com/TheAndromedaCat/Grab-UI"
-  url "https://github.com/TheAndromedaCat/Grab-UI/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "6a3947768c8afa6cb83e00676fc221494078343eeedfba6141a24e169c6b69e3"
+  url "https://github.com/TheAndromedaCat/Grab-UI/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "4542caa5a565d871f26716059807b835251e634e726fc9c60007c18144e00f3a"
   license "MIT"
 
   depends_on "node"
@@ -19,11 +19,30 @@ class GrabUi < Formula
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
+  service do
+    run [opt_bin/"grab-ui"]
+    keep_alive true
+    error_log_path var/"log/grab-ui.log"
+    log_path var/"log/grab-ui.log"
+    working_dir var/"grab-ui"
+  end
+
+  def post_install
+    (var/"grab-ui").mkpath
+    (var/"log").mkpath
+  end
+
   def caveats
     <<~EOS
       Grab-UI requires PAM for authentication on Linux. Ensure 'libpam0g-dev' (Debian/Ubuntu) 
       or 'pam-devel' (RHEL/Fedora) is installed on your host system.
       
+      To start Grab-UI as a background service:
+        brew services start grab-ui
+
+      Alternatively, you can use PM2 to manage the process:
+        pm2 start #{opt_bin}/grab-ui --name grab-ui
+
       Optional dependencies like 'handbrake-cli' and 'filebot' should be installed separately:
         brew install handbrake-cli (Linux) or brew install --cask handbrake-cli (macOS)
     EOS
